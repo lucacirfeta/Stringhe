200bb7ffaf84ae81749fc981c234b2728a6ca70c
alessio.cirfeta@gmail.com <Cirfet@93>
2024-04-11 23:07:38 +0200

    lok

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner s = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Digita la prima parola: ");
+        String prima = s.nextLine();
+        System.out.print("Digita la seconda parola: ");
+        String seconda = s.nextLine();
+
+        System.out.println("--------------------------------------");
+
+        Main m = new Main();
+
+        m.confronto(prima, seconda);
+        m.concat(prima, seconda);
+        m.convert(prima, seconda);
+    }
+
+    public void confronto(String a, String b) {
+        System.out.println("Esegue il confronto");
+        System.out.println("Le due parole sono uguali? " + a.equals(b));
+
+        System.out.println("--------------------------------------");
+
+    }
+
+    public void concat(String a, String b) {
+        System.out.println("Esegue la concatenazione");
+        System.out.println(a + " e " + b + " concatenate le due parole formano formano: " + a.concat(b));
+
+        System.out.println("--------------------------------------");
+
+    }
+
+    public void convert(String a, String b) {
+        System.out.println("Esegue la conversione");
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Vuoi convertire le parole in maiuscolo o in minuscolo?");
+        String scelta = s.nextLine();
+
+        switch (scelta) {
+            case "maiuscolo" -> {
+                System.out.println(a.toUpperCase());
+                System.out.println(b.toUpperCase());
+            }
+            case "minuscolo" -> {
+                System.out.println(a.toLowerCase());
+                System.out.println(b.toLowerCase());
+            }
+
         }
+
     }
 }
